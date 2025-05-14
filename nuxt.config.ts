c2e38ee86d5524efd722e5f06d11d2f97df1de9f
dmkurt <dmkurt@taltech.ee>
2025-05-14 21:34:04 +0300

    Created main weather component with temperature, description, and icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: false },
   runtimeConfig: {
     public: {
-      openweatherApiKey: process.env.OPENWEATHER_API_KEY
-    }
-  }
-})
+      openweatherApiKey: process.env.OPENWEATHER_API_KEY,
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const city = "Tallinn";
+
+const { data, error, loading } = useWeather(city);
+</script>
+
+<template>
+  <h1>Weather in {{ city }}</h1>
+  <div v-if="loading">Loading...</div>
+  <div v-if="error">Error: {{ error }}}</div>
+  <div v-if="data">
+    <img
+      :src="`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`"
+      alt="Weather Icon"
+      class="weather-icon"
+    />
+    <p>{{ Math.round(data.main.temp) }}°C</p>
+    <p>{{ data.weather[0].description }}</p>
+    <p>{{ new Date().toLocaleDateString() }}</p>
+  </div>
+</template>
+
+<style scoped>
+.weather-icon {
+  width: 100px;
+  height: 100px;
+  margin-bottom: 10px;
+}
+</style>
